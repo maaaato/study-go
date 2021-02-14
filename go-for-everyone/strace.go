e57c44003d0e3a224d089dd106cc1264052d1f01
maaaato <igosso.mato@gmail.com>
2021-02-14 22:50:23 +0900

    [Update]use bufio

--- go-for-everyone/strace.go
+++ go-for-everyone/strace.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
+	b := bufio.NewWriter(os.Stdout)
 	for i := 0; i < 100; i++ {
-		fmt.Fprintln(os.Stdout, strings.Repeat("x", 100))
+		fmt.Fprintln(b, strings.Repeat("x", 100))
 	}
+	b.Flush()
 }
