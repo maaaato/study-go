b248fa8086242a52ba55d7af8331a29d1f851b33
maaaato <igosso.mato@gmail.com>
2021-04-19 00:09:25 +0900

    [Add] mapのExampleを追加

--- go-for-everyone/6.1/calc_test.go
+++ go-for-everyone/6.1/calc_test.go
@@ -1,23 +1,34 @@
 package calc
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestSum(t *testing.T) {
-	if sum(1, 2) != 3{
+	if sum(1, 2) != 3 {
 		t.Fatal("sum(1,2) should be 3, but doesn't match")
 	}
 }
 
 func TestSumFatal(t *testing.T) {
-	if sum(1, 3) != 3{
+	if sum(1, 2) != 3 {
 		t.Fatal("sum(1,2) should be 3, but doesn't match")
 	}
 }
 
 func ExampleHello() {
-    fmt.Println("Hell")
-   // Output: Hello
+	fmt.Println("Hello")
+	// Output: Hello
+}
+
+func ExampleShuffleWillBeFailed() {
+	x := map[string]int{"a": 1, "b": 2, "c": 3}
+	for k, v := range x {
+		fmt.Printf("k=%s v=%d\n", k, v)
+	}
+	// Unordered output:
+	// k=a v=1
+	// k=b v=2
+	// k=c v=3
 }
