b89da0a9b9cc320596bbdd0a2247b672f294d189
maaaato <igosso.mato@gmail.com>
2022-01-13 22:57:02 +0900

    [Add] cat command

--- gopher-dojo/5/main.go
+++ gopher-dojo/5/main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
-	"strings"
+	"os"
 )
 
-var msg = flag.String("msg", "default", "説明")
-
-var n int
+var n bool
 
 func init() {
-	flag.IntVar(&n, "n", 1, "回数")
+	flag.BoolVar(&n, "n", false, "Output line")
 }
 
 func main() {
 	flag.Parse()
-	var files = flag.Arg(0)
-	fmt.Println(strings.Repeat(*msg, n))
-	fmt.Println(files)
+	var files = flag.Args()
+	var line int
+	for _, v := range files {
+		f, err := os.Open(v)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "can't open file:", err)
+		}
+		scanner := bufio.NewScanner(f)
+		for scanner.Scan() {
+			line += 1
+			if n {
+				fmt.Fprintln(os.Stdout, line, ":", scanner.Text())
+			} else {
+				fmt.Fprintln(os.Stdout, scanner.Text())
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+	}
 }
